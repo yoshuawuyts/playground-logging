3349a13efc24914c0fde7e8da3841babe4befbae
Yoshua Wuyts <i@yoshuawuyts.com>
2015-11-03 16:38:40 +1100

    error: add

--- app-main/index.js
+++ app-main/index.js
@@ -4,16 +4,18 @@ const sizeStream = require('size-stream')
 const summary = require('server-summary')
 const stdout = require('stdout-stream')
 const pumpify = require('pumpify')
+const typedError = require('error/typed')
 const bole = require('bole')
 const http = require('http')
 
 const port = 1337
 
 // set bole output level
-bole.output({ level: 'info', stream: stdout })
+bole.output({ level: 'debug', stream: stdout })
 
 // create reusable log stream for ndjson
 const logStream = boleStream({ level: 'debug' })
+const log = bole('app-main')
 
 const server = http.createServer(function (req, res) {
   // create logger
@@ -24,6 +26,17 @@ const server = http.createServer(function (req, res) {
   const size = sizeStream()
   size.once('size', size => httpLogger.setContentLength(size))
   const sink = pumpify(size, res)
+
+  //
+  // DELETE ME
+  //
+  var serverError = typedError({
+    type: 'server.5xx',
+    message: '{title} server error, status={statusCode}',
+    statusCode: 500
+  })
+  const foobar = serverError({ message: 'whoopsie' })
+  log.info(foobar)
 
   // start logic
   sink.end('hello')
